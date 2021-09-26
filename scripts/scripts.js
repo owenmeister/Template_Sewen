f0cc891a408feaa63a95031a9ecd7595c920b34a
owenmeister <91085193+owenmeister@users.noreply.github.com>
2021-09-26 17:59:33 +0200

    Initial commit

---
+++ scripts/scripts.js
@@ -0,0 +1,76 @@
+const menu = document.querySelector("#menu");
+
+const navMenu = document.querySelector(".navbar-menu");
+const menuToggler = document.querySelector(".menu-toggler");
+
+const navbar = document.querySelector("nav");
+
+
+menuToggler.addEventListener("click", () => {
+	if(menuToggler.classList.contains("active")) {
+		menuToggler.classList.remove("active");
+		navMenu.classList.remove("active");
+	} else {
+		menuToggler.classList.add("active");
+		navMenu.classList.add("active");
+	}
+});
+
+function showSlides(n) {
+	var i;
+	var slides = document.getElementsByClassName("mySlides");
+	var dots = document.getElementsByClassName("dot");
+	if (n > slides.length) {slideIndex = 1}
+	  if (n < 1) {slideIndex = slides.length}
+	  for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	  }
+	  for (i = 0; i < dots.length; i++) {
+		dots[i].className = dots[i].className.replace(" active-slide", "");
+	  }
+	slides[slideIndex-1].style.display = "flex";
+	dots[slideIndex-1].className += " active-slide";
+}
+
+addEventListener("scroll", () => {
+	let scroll = this.scrollY;
+	if(scroll > menu.clientHeight) {
+    go2Top.style.opacity = 1;
+    go2Top.style.lineHeight = "40px";
+		go2Top.style.cursor = "pointer";
+		console.log(go2Top.style.cursor);
+	} else {
+		go2Top.style.opacity = 0;
+		go2Top.style.cursor = "default";
+		console.log(go2Top.style.cursor);
+	}
+})
+
+go2Top.addEventListener("click", () => {
+	if(go2Top.style.opacity != 0) window.scrollTo(0, 0);
+})
+
+document.addEventListener("DOMContentLoaded", function() { startplayer(); }, false);
+var player;
+
+function startplayer() 
+{
+ player = document.getElementById('video_player');
+ player.controls = false;
+}
+function play_vid()
+{
+ player.play();
+ document.querySelector(".playbutton").style.opacity = "0";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
